36fa4f3baa5be0373a3e29385db07cc9299a3f18
cleanroom-dev <dev@cleanroom.local>
2026-08-20 20:06:24 +0800

    refactor: internal adjustment

--- internal/service/report_svc.go
+++ internal/service/report_svc.go
@@ -85,9 +85,7 @@ func (s *ReportService) Trend(ctx context.Context, roomID int64, paramType strin
 			})
 		}
 	}
-	if len(res.Points) > limit {
-		res.Points = res.Points[len(res.Points)-limit:]
-	}
+	res.Points = res.Points[len(res.Points)-limit:]
 	return res, nil
 }
 
